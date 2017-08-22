95e76fc9ed46798b14fe8ed578578c31a6aa6884
Caco V. Topia <sinosie7en@gmail.com>
2017-08-22 17:30:12 +0800

    example

---
+++ src/main/java/reactivex/examples/SynchronousObservable.java
@@ -0,0 +1,14 @@
+package reactivex.examples;
+
+import rx.Observable;
+
+public class SynchronousObservable {
+
+
+    public static void main(String[] args) {
+
+        Observable.create(aSubcriber ->{
+
+        });
+    }
+}
